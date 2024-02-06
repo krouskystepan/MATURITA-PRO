8f134443bd5e04a7f3717da88d7e1ab9527793cc
Stepan Krousky <stepan.krousky@seznam.cz>
2024-02-06 21:59:17 +0100

    Otazka 19

---
+++ Otazka_19/src/app.js
@@ -0,0 +1,14 @@
+/*
+Zadani:
+Vytvořte aplikaci, ve které bude input pro vložení textu a tlačítko. Po stisknutí tlačítka se napsaný text vloží na stránku. Projekt exportujte do spustitelného souboru.
+- Musite si to buildnout sami (ja to na macu pro win nebudu delat)
+    - npm run build
+*/
+
+const button = document.getElementById('button');
+const input = document.getElementById('input');
+const text = document.getElementById('text');
+
+button.onclick = () => {
+    text.innerText = input.value;
+}
