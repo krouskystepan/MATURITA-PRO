274b29b3b20931bc6864c03e7997455e286700e1
Stepan Krousky <stepan.krousky@seznam.cz>
2024-02-07 15:41:28 +0100

    Otazka 18

--- Otazka_18/src/main/java/Main.java
+++ Otazka_18/src/main/java/Main.java
@@ -2,13 +2,28 @@ import java.sql.SQLException;
 
 /*
 Zadani:
-
+18.	Vytvořte třídu pro práci s databází. V této třídě vytvořte metodu pro připojení k databázi, kterou vyvoláte po spuštění programu. Vytvořte metodu pro výpis z tabulky, metodu pro vložení dat do tabulky. Vytvořte na serveru databázi a tabulku, která bude mít alespoň tři sloupce. Podle této tabulky vytvořte v Javě příkazy pro práci s databází a ověřte funkčnost.
 
 Postup:
 XAMPP: V:\Vyuka_IT\WEB_APLIKACE
 1. Extrahujeme na plochu
 2. Pustime setup_xampp.bat
-3. Pustime xampp.control.exe
+3. Pustime xampp_control.exe
+4. Startneme apache a mysql
+5. Klikneme na mysql admin
+
+phpMyAdmin
+1. Vytvorime DB s nazvem test
+2. V ni tabulku users
+3. Sloupce
+    - id int AUTO_INCREMENT
+    - firstName VARCHAR 64
+    - lastName VARCHAR 64
+    - age int
+
+-----
+1. Zkusime to tady pustit
+2. Pokud tady nemame chybu, tak bychom meli v DB mit zapis
 */
 public class Main {
 
