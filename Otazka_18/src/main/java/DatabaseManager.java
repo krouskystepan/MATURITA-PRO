49393c5e205913e27ddc97ff09b6a23c032ae5eb
Stepan Krousky <stepan.krousky@seznam.cz>
2024-02-07 15:32:15 +0100

    Otazka 18

--- Otazka_18/src/main/java/DatabaseManager.java
+++ Otazka_18/src/main/java/DatabaseManager.java
@@ -10,13 +10,13 @@ public class DatabaseManager {
         user: přihlašovací jméno, v xamppu root
         password: heslo, v xamppu není nastaveno
          */
-        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/database","root","");
+        Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
         /*
         Pro vykonání příkazu potřebujeme vytvořit PreparedStatement
         V inicializaci použijeme vytvořeme připojení a připravíme statement
         Do závorky píšeme SQL příkaz
          */
-        PreparedStatement statement = c.prepareStatement("SELECT * FROM zoo");
+        PreparedStatement statement = c.prepareStatement("SELECT * FROM users");
         /*
         Pokud používáme příkaz, který nám vrací nějaký data, potřebujeme použít ResultSet, abychom mohli s těmito daty pracovat
         Ze statementu vyvoláme executeQuery()
@@ -30,24 +30,24 @@ public class DatabaseManager {
             Z ResultSetu můžeme používat celou řadu metod pro získání konkrétní hodnoty
             Do závorek píšeme název sloupce databáze
              */
-            System.out.println(set.getString("column1"));
-            System.out.println(set.getInt("column2"));
-            System.out.println(set.getString("column3"));
+            System.out.println(set.getString("firstName"));
+            System.out.println(set.getString("lastName"));
+            System.out.println(set.getInt("age"));
         }
     }
 
-    public static void setData(String val1, int val2, String val3) throws SQLException{
+    public static void setData(String firstName, String lastName, int age) throws SQLException{
         Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/database", "root", "");
         /*
         Pokud do příkazu dosazujeme konkrétní data, nedáváme je přímo do prepareStatement, ale nahradíme daná místa otazníky a konkrétní hodnoty dosadíme na dalších řádcích
          */
-        PreparedStatement statement = c.prepareStatement("INSERT INTO zoo VALUES (NULL,?,?,?)");
+        PreparedStatement statement = c.prepareStatement("INSERT INTO users VALUES (NULL,?,?,?)");
         /*
         Pomocí metod setString, setInt, ... dosazujeme konkrétní hodnoty za otazníky v příkazu. Indexace od 1
          */
-        statement.setString(1, val1);
-        statement.setInt(2, val2);
-        statement.setString(3, val3);
+        statement.setString(1, firstName);
+        statement.setString(2, lastName);
+        statement.setInt(3, age);
         statement.execute(); //Nevracíme žádná data, se kterýma chceme pracovat, proto dáme pouze execute
     }
 }
--- Otazka_18/src/main/java/Main.java
+++ Otazka_18/src/main/java/Main.java
@@ -8,13 +8,13 @@ Postup:
 XAMPP: V:\Vyuka_IT\WEB_APLIKACE
 1. Extrahujeme na plochu
 2. Pustime setup_xampp.bat
-3. 
+3. Pustime xampp.control.exe
 */
 public class Main {
 
     public static void main(String[] args) {
         try {
-            DatabaseManager.setData("jjj", 44, "white");
+            DatabaseManager.setData("Stepan", "Krousky", 19);
         } catch (SQLException e) {
             e.printStackTrace();
         }
