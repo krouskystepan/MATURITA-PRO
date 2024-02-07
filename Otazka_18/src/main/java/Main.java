838050ae7c7b600edcadd41a423932d9dac3b0cb
Stepan Krousky <stepan.krousky@seznam.cz>
2024-02-07 11:22:53 +0100

    Otazka 18

--- Otazka_18/src/main/java/Main.java
+++ Otazka_18/src/main/java/Main.java
@@ -1,5 +1,15 @@
 import java.sql.SQLException;
 
+/*
+Zadani:
+
+
+Postup:
+XAMPP: V:\Vyuka_IT\WEB_APLIKACE
+1. Extrahujeme na plochu
+2. Pustime setup_xampp.bat
+3. 
+*/
 public class Main {
 
     public static void main(String[] args) {
