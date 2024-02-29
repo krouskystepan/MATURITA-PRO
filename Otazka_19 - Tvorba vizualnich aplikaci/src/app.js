1a0f8e5d8dc16686d4bb955768e66891d744e3a3
stepan.krousky <stepan.krousky@seznam.cz>
2024-02-29 09:58:10 +0100

    Otazka 19 fix

--- Otazka_19 - Tvorba vizualnich aplikaci/src/app.js
+++ Otazka_19 - Tvorba vizualnich aplikaci/src/app.js
@@ -2,7 +2,7 @@
 Zadani:
 Vytvořte aplikaci, ve které bude input pro vložení textu a tlačítko. Po stisknutí tlačítka se napsaný text vloží na stránku. Projekt exportujte do spustitelného souboru.
 - Musite si to buildnout sami (ja to na macu pro win nebudu delat)
-    - npm run build
+    - npm run make
 */
 
 const button = document.getElementById('button');
